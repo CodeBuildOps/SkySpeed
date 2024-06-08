8586f21f4a218444c65b27ec3f97356e6487aecd
Abhishek Kumar Singh <abhishek.singh@hexagon.com>
2024-06-08 10:41:22 +0000

    Merged PR 69: Handled printing and sending email with correct passengers data - Client
    Handled printing and sending email with correct passengers data - Client

--- Sources/Client/Sources/EndRecord/EndRecordPage.xaml.cs
+++ Sources/Client/Sources/EndRecord/EndRecordPage.xaml.cs
@@ -1,8 +1,14 @@
-﻿using SkySpeed.MessageLog;
+﻿using SharedData;
+using SkySpeed.FlightResults;
+using SkySpeed.MessageLog;
+using SkySpeed.Passengers;
 using SkySpeedService;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace SkySpeed.EndRecords
@@ -14,20 +20,35 @@ namespace SkySpeed.EndRecords
     {
         private readonly DisplayMessage _displayMessage;
         private SkySpeedServices _skySpeedServices;
-        private readonly string _toMail;
+        private const string _docxFile = "PNR.docx";
+        private string _toMail;
+        private List<string> _passengerNames;
+        private List<string> _seats;
+        private string _pnr;
+        private string _flightNumber;
+        private string _flightDuration;
+        private string _takeOff;
+        private string _takeOffAirport;
+        private string _landing;
+        private string _landingAirport;
 
         public EndRecordPage()
         {
             InitializeComponent();
 
+            _passengerNames = new List<string>();
+            _seats = new List<string>();;
             _displayMessage = new DisplayMessage("End record");
-            _toMail = GetEmailId();
+            GetEmailId();
+            GenerateRandomPNR(6);
+            GetAllPassengersDetails();
+            GetFlightDetails();
         }
 
-        private string GetEmailId()
+        private void GetEmailId()
         {
             // Todo: Get email id from datagrid.
-            return "abhiksingh1999@gmail.com";
+            _toMail = "abhiksingh1999@gmail.com";
         }
 
         private void SendItineraryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -36,14 +57,69 @@ namespace SkySpeed.EndRecords
                 return;
 
             _skySpeedServices = new SkySpeedServices();
+            string htmlContent = _skySpeedServices.GenerateHtml(
+                _passengerNames,
+                _takeOff,
+                _takeOffAirport,
+                _landing,
+                _landingAirport,
+                _flightDuration,
+                _flightNumber,
+                _seats,
+                _pnr
+                );
             switch (selectedItemControl.Content.ToString().ToUpper())
             {
                 case "EMAIL":
-                    HandleEndRecordSelection("Would you like to send an email?", () => _skySpeedServices.SendEmail(_toMail));
+                    HandleEndRecordSelection("Would you like to send an email?", () => _skySpeedServices.SendEmail(_toMail, htmlContent));
                     break;
                 case "PRINT":
-                    HandleEndRecordSelection("Would you like to print?", () => _skySpeedServices.PrintDocument());
+                    HandleEndRecordSelection("Would you like to print?", () => _skySpeedServices.PrintDocument(Path.Combine(Environment.CurrentDirectory, _docxFile), htmlContent));
                     break;
+            }
+        }
+
+        private void GenerateRandomPNR(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            char[] pnrArray = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                pnrArray[i] = chars[random.Next(chars.Length)];
+            }
+
+            _pnr = new string(pnrArray);
+        }
+
+        private void GetAllPassengersDetails()
+        {
+            if (SharedDataPage.PassengersDetailsGrid?.Items != null)
+            {
+                foreach (var rowItem in SharedDataPage.PassengersDetailsGrid.Items)
+                {
+                    if (rowItem is PassengersDetails row)
+                    {
+                        _passengerNames.Add(row.FullName);
+                        _seats.Add(row.Seat);
+                    }
+                }
+            }
+        }
+
+        private void GetFlightDetails()
+        {
+            if (SharedDataPage.FlightDetails is FlightDetails flightDetails)
+            {
+                _flightNumber = flightDetails.FlightNumber;
+                _flightDuration = flightDetails.Duration;
+
+                _takeOff = $"{flightDetails.DayAndDate.Trim()}, {flightDetails.DepartArrival.Split('-')[0].Trim()}";
+                _takeOffAirport = flightDetails.Sector.Split('-')[0];
+
+                _landing = $"{flightDetails.DayAndDate.Trim()}, {flightDetails.DepartArrival.Split('-')[1].Trim()}";
+                _landingAirport = flightDetails.Sector.Split('-')[1];
             }
         }
 
